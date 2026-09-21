d817e34277a52aa0077d1d1b05254f418141ea54
Ramadanski <uward@abv.bg>
2026-09-21 14:27:02 +0300

    Format Crystal source files

--- spec/chemistry_spec.cr
+++ spec/chemistry_spec.cr
@@ -1,15 +1,15 @@
 require "./spec_helper"
 
 describe Chemistry do
-it "contains Hydrogen" do
-Chemistry::ELEMENTS["H"][:name].should eq("Hydrogen")
-end
+  it "contains Hydrogen" do
+    Chemistry::ELEMENTS["H"][:name].should eq("Hydrogen")
+  end
 
-it "contains Carbon" do
-Chemistry::ELEMENTS["C"][:mass].should eq(12.011)
-end
+  it "contains Carbon" do
+    Chemistry::ELEMENTS["C"][:mass].should eq(12.011)
+  end
 
-it "contains Iron" do
-Chemistry::ELEMENTS["Fe"][:number].should eq(26)
-end
+  it "contains Iron" do
+    Chemistry::ELEMENTS["Fe"][:number].should eq(26)
+  end
 end
